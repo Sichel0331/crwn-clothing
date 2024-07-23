884a36cb1a7e020527551a50f0ca6f68ce4e1d5c
Sichel <jonnyexclusiv32@googlemail.com>
2024-07-23 13:14:53 +0200

    Including ROutes and implementing them, adding NavBar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
@@ -7,7 +8,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
