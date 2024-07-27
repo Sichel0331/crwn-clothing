a1a62b90b77d7cccf4bd02893415cdd49d8ffd27
Sichel <jonnyexclusiv32@googlemail.com>
2024-07-27 22:43:12 +0200

    Implemented SignInWithGoogleRedirect

--- src/routes/signin/signin.component.jsx
+++ src/routes/signin/signin.component.jsx
@@ -1,19 +1,27 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
-
-const SignIn= () => {
+import {
+    signInWithGooglePopup,
+    signInWithGoogleRedirect,
+    createUserDocumentFromAuth,
+  } from '../utils/firebase/firebase.utils';
+  
+  const SignIn = () => {
     const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup();
-        createUserDocumentFromAuth(user); 
-    }
-    
-    return(
-        <div>
-            <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>
-                Mit Google Popup anmelden
-            </button>
-        </div>
-    )
-}
+      const { user } = await signInWithGooglePopup();
+      const userDocRef = await createUserDocumentFromAuth(user);
+    };
 
-export default SignIn;
+    const logGoogleRedirectUser = async () => {
+        const { user } = await signInWithGoogleRedirect();
+        console.log({user});
+      };
+  
+    return (
+      <div>
+        <h1>Sign In Page</h1>
+        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+        <button onClick={logGoogleRedirectUser}>Sign in with Google Redirect</button>
+      </div>
+    );
+  };
+  
+  export default SignIn;
