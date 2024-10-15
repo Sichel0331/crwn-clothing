7ee965e9d95d9a4a26d8822c0b9585c87e480d59
Sichel <jonnyexclusiv32@googlemail.com>
2024-10-15 14:13:25 +0200

    rewrote buttonsContainer in sign-in-form.styles.jsx

--- src/components/sign-in-form/sign-in-form.styles.jsx
+++ src/components/sign-in-form/sign-in-form.styles.jsx
@@ -9,7 +9,7 @@ width:380px;
 export const H2 = styled.div`
 margin: 10px 0;
 `
-export const ButtonContainer = styled.div`
+export const ButtonsContainer = styled.div`
     display: flex;
     justify-content: space-between;
-`
+    `
